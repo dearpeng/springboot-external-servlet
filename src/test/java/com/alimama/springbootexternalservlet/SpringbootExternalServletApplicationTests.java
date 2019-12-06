b7339b37e686c25bb1b339fabada70bea9d13512
pengwenxiang <pengwenxiang@fincs.com.cn>
2019-12-06 18:09:35 +0800

    111

---
+++ src/test/java/com/alimama/springbootexternalservlet/SpringbootExternalServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alimama.springbootexternalservlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExternalServletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
